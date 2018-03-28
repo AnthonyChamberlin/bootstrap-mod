1075573a746c1b0ab4af771c437653c99977e688
Anthony Chamberlin <anthony@mnadigital.co.uk>
2018-03-28 10:26:23 +0100

    fix gallery scale ups

--- js/gallery.js
+++ js/gallery.js
@@ -22,8 +22,12 @@ function openModal() {
     let slides = document.getElementsByClassName("slides");
     let dots = document.getElementsByClassName("demo");
     let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+      slideIndex = 1
+    } else if (n < 1) {
+      slideIndex = slides.length
+    }
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
